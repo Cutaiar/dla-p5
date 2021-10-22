053f26d006d4b495451cdd0807f9c44d2d88c8de
Dillon Cutaiar <dillonc@vt.edu>
2021-10-21 21:48:02 -0600

    store configs

--- src/app.ts
+++ src/app.ts
@@ -2,44 +2,43 @@ import P5 from "p5";
 import "p5/lib/addons/p5.dom";
 
 import { Node, createRandomNodes } from "./Node";
-import { getSeed } from "./Seed";
+import { getSeed, SeedConfig } from "./Seed";
 
 // simulation parameters
-const numberOfNodes = 1000;
-const nodeDiameter = 15;
-const energy = 2;
+const numberOfNodes = 3000;
+const nodeDiameter = 10;
+const energy = 1;
 
 // todo repulse
 // or todo bounce
 var sketch = (p: P5) => {
   let nodes: Node[];
 
+  const configurations: { [key: string]: SeedConfig } = {
+    point: {
+      p: p,
+      type: "point",
+      diameter: nodeDiameter,
+      energy: energy,
+      x: p.width / 2,
+      y: p.height / 2,
+    },
+    line: {
+      p: p,
+      type: "line",
+      diameter: nodeDiameter,
+      energy: energy,
+      y: p.height + 10,
+      density: 1,
+    },
+  };
+
   p.setup = () => {
     p.createCanvas(p.windowWidth, p.windowHeight);
     nodes = createRandomNodes(p, numberOfNodes, nodeDiameter, energy);
 
     // Get a seed and add it to nodes
-    nodes.push(
-      ...getSeed({
-        p: p,
-        type: "point",
-        diameter: nodeDiameter,
-        energy: energy,
-        x: p.width / 2,
-        y: p.height / 2,
-        // density: 1,
-      })
-    );
-    // nodes.push(
-    //   getSeed({
-    //     p: p,
-    //     type: "point",
-    //     x: p.width / 2,
-    //     y: p.height / 2,
-    //     diameter: nodeDiameter,
-    //     energy: energy,
-    //   })
-    // );
+    nodes.push(...getSeed(configurations["line"]));
   };
 
   p.windowResized = () => {
